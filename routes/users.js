443adfbfd26d4eb939c9f3e53b75a95b06c042d8
Valérie <valbrousse@gmail.com>
2022-11-24 17:53:10 +0100

    save

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 require('../models/connection');
-const User = require('../models/users');
+const User = require('../models/user');
 const { checkBody } = require('../modules/checkBody');
 
 router.post('/signup', (req, res) => {
