c2a6dbe81bcac384fe37355f50d7b8463ee55b03
Valérie <valbrousse@gmail.com>
2022-11-25 09:56:47 +0100

    pu

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var tweetsRouter = require('./routes/users');
+var tweetsRouter = require('./routes/tweets');
 
 var app = express();
 const cors = require('cors');
--- routes/tweets.js
+++ routes/tweets.js
@@ -3,19 +3,21 @@ var router = express.Router();
 require('../models/connection');
 
 const fetch = require('node-fetch');
+const { find } = require('../models/users');
 
 const CONNECTION_STRING = process.env.CONNECTION_STRING;
 
 //Récupération des tweets
 router.get('/tweet', (req, res) => {
-  fetch(`https://${CONNECTION_STRING}/tweets`)
+  fetch(`https://${CONNECTION_STRING}`)
+  Tweet.find({username: req.body.username})
     .then(response => response.json())
     .then(data => {
       let message = req.body.message;
       if (message.length > 280) {
         message = message.substring(0, 280) + '...';
       }
-      res.json({message});
+      res.json({message: data});
       return;
     });
 });
