4fb58b9833c1a8c4cbef27df260be25fab372054
Valérie <valbrousse@gmail.com>
2022-11-25 12:14:40 +0100

    pause midiB

--- models/connection.js
+++ models/connection.js
@@ -6,4 +6,6 @@ mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
 
+
+
   
--- routes/tweets.js
+++ routes/tweets.js
@@ -1,28 +1,17 @@
 var express = require('express');
 var router = express.Router();
-require('../models/connection');
-
-const fetch = require('node-fetch');
-const { find } = require('../models/users');
-
-const CONNECTION_STRING = process.env.CONNECTION_STRING;
+var Tweet = require('../models/tweets');
 
 //Récupération des tweets
 router.get('/tweet', (req, res) => {
-  fetch(`https://${CONNECTION_STRING}`)
   Tweet.find({username: req.body.username})
-    .then(response => response.json())
     .then(data => {
-      let message = req.body.message;
-      if (message.length > 280) {
-        message = message.substring(0, 280) + '...';
-      }
-      res.json({message: data});
-      return;
+      let message = data.message;
+      res.json({message: data.message});
+      return {message};
     });
 });
 
 module.exports = router;
-
 
 
